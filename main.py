9cb13677c0ac9bb804fc928690b696f2ed03f626
DANIELA GOMEZ PENICHE <danygp1999@gmail.com>
2023-01-31 13:54:57 -0600

    Leer archivos .doc

--- main.py
+++ main.py
@@ -8,11 +8,29 @@ from nltk.tokenize import WordPunctTokenizer #nos permite separar el texto en to
 import matplotlib
 from nltk.stem import PorterStemmer #stem es un sub-paquete de nltk que elimina los afijos morfológicos (producto extra) para una cadena o contenidos
                                     #el algoritmo porter stemmer hace el stemming
+import aspose.words as aw #nos permmite leer archivos .doc
 
-f = open("C:/Users/danyg/Documents/Internado/Pacientes/PACIENTE_1.txt","r", encoding='utf-8-sig') #abrir el archivo del paciente
-lines = len(f.readlines()) #encontrar el numero de líneas en el documento
-f.seek(0) #volver al inicio del archivo
-text = f.readlines() #leer el contenido del archivo
-print(text[2:30]) #imprimir ciertas lineas del archivo
-print(lines)
-f.close() #cerrar el archivo
+#Variables globales
+f = []
+
+# Cargar el archivo a leer
+doc = aw.Document(r"C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.doc")
+
+# Leer el contenido de los parrafos tipo nodo
+for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
+    paragraph = paragraph.as_paragraph()
+    f.append(paragraph.to_string(aw.SaveFormat.TEXT))
+
+print(len(f))
+print(f[3:5])
+
+
+
+
+
+#name = r'C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.doc'
+#out = pypandoc.convert_file(name, 'plain', outputfile=r"C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.txt")
+#assert out == ""
+#text = txt.process(r"C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.DOC", encoding="UTF-8")
+#text = text.decode("utf8")
+#text = textract.process("C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.DOC", encoding = 'unicode_escape')
