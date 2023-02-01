906bb2887cccc336bc62ce922120100cdb8345d5
DANIELA GOMEZ PENICHE <danygp1999@gmail.com>
2023-02-01 13:08:50 -0600

    Manejo de HTML con Flask

--- test.py
+++ test.py
@@ -11,18 +11,17 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/success/<name>')
 def success(name):
-    return 'welcome %s' % name
- 
+    return 'Si sirvio %s' % name
  
 @app.route('/index', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
+        user = request.form['myfile']
+        return redirect(url_for('success', name=user))    
     else:
-        user = request.args.get('nm')
+        user = request.args.get('myfile')
         return redirect(url_for('success', name=user))
- 
+
 # main driver function
 if __name__ == '__main__':
  
