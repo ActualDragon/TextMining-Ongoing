dc7f935b26b9f79f29a09ef94e1a8adbd98fca14
DANIELA GOMEZ PENICHE <danygp1999@gmail.com>
2023-02-20 13:10:49 -0600

    Econtrar edad y probar NLTK

--- main.py
+++ main.py
@@ -1,31 +1,116 @@
-import codecs #implementar una clase para codificar y decodificar archivos y provee acceso para administrar errores en el proceso de busqueda
-import collections #alternativa para contenedores como contenedores como dictados, listas, tuplas y conjuntos.
- 
-import numpy as np
-import pandas as pd #manipulacion y analisis de datos
+from flask import Flask, redirect, url_for, request, render_template, session, abort #Framework que permite crear aplicaciones web
+from werkzeug.utils import secure_filename #validar el archivo -> "Never trust user input"
+import os #usar funcionalidades dependientes del sistema operativo
+import aspose.words as aw #Lectura de archivos
+import webbrowser #Manejar el navegador
+import filetype
 import nltk #libreria usada para procesamiento de lenguaje natural
-from nltk.tokenize import WordPunctTokenizer #nos permite separar el texto en tokens
-import matplotlib
-from nltk.stem import PorterStemmer #stem es un sub-paquete de nltk que elimina los afijos morfológicos (producto extra) para una cadena o contenidos
-                                    #el algoritmo porter stemmer hace el stemming
-import aspose.words as aw #nos permmite leer archivos .doc
+from nltk.corpus import wordnet as wn
 
-"""
-Importar librerias de python
-En terminal de VS Code:  py -m pip install libreria
-En terminal Python: import libreria
-                    print(libreria.__file__)
-"""
-#Variables globales
-f = []
+#Validar que el archivo sea del tipo permitido (sin importar la extension que tenga)
+def validate_file(file):
+    header = file.read()
+    file.seek(0)
+    format = filetype.guess(header)
+    if (format is None):
+        return None
+    format = format.extension
+    if (format != "doc") and (format != "docx"):
+        return None
+    return '.' + format
 
-# Cargar el archivo a leer
-doc = aw.Document(r"C:/Users/danyg/Documents/Internado/TextMining/Pacientes/PACIENTE-2.doc")
+#Leer la copia local del archivo que se subió
+def Read_File(name):
+    f = []
+    text = []
+    basedir = os.path.abspath(os.path.dirname(__file__)) #Obtener el directorio actual
+    path = "".join([basedir,"\\static\\uploads\\", name]) #Obtener el directorio del archivo temporal
+    doc = aw.Document(path) # Cargar el archivo a leer
+    # Leer el contenido de los parrafos tipo nodo
+    for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
+        paragraph = paragraph.as_paragraph()
+        p = paragraph.to_string(aw.SaveFormat.TEXT)
+        p = p.replace("\\", "/").replace('"','\\"').replace("'","\'") #Escapar caracteres especiales
+        p = p.replace('\n', '').replace('\r', '') #Eliminar saltos de linea y el retorno de carro
+        f.append(p)
+    #Eliminar la copia temporal del archivo
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print("El archivo no existe")
+    #Eliminar el texto adicional que agrega la libreria aspose.words
+    size = len(f)
+    for x in range(1,size-2):
+        text.append(f[x])
+    return text
 
-# Leer el contenido de los parrafos tipo nodo
-for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
-    paragraph = paragraph.as_paragraph()
-    f.append(paragraph.to_string(aw.SaveFormat.TEXT))
+#Encontrar la edad del paciente
+def Edad(f):
+    j = ""
+    edad = []
+    #De acuerdo con el analisis de la estructura de los expedientes, la edad siempre se encuentra antes del tag "ANTECEDENTES"
+    for x in range(len(f)):
+        i = f[x].find("ANTECEDENTES")
+        if i != -1:
+            j =  x #Encontrar el elemento de la lista donde empiezan los antecedentes (pues la edad va a estar antes)
+    if j != "":
+        for x in range(j):
+            k = f[x].find("A\u00d1OS")
+            if k != -1:
+                k = f[x].find("MESES") #Validar su el paciente tiene menos de un año de edad
+                if k != -1:
+                    l =  x #Elemento de la lista que contiene la edad
+                    edad = [int(i) for i in f[l].split() if i.isdigit()]
+            else:
+                edad.append(0)
+    else:
+        edad.append(0)
+    return edad
 
-print(len(f))
-print(f[3:5])
+# _.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.
+
+# CONSTRUCTOR DE FLASH
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 #Limitar archivos a maximo 1MB
+app.config['UPLOAD_PATH'] = r'./static/uploads' #Path al que se subira la copia temporal de los archivos a ser procesados
+app.config['UPLOAD_EXTENSIONS'] = ['.doc', '.docx'] #Extensiones permitidas
+
+#Generar la home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+#Recibir el archivo subido
+@app.route('/index', methods=['POST'])
+def load():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename) #validar el nombre del archivo 
+    if filename != '': #validar que si se subió un archivo
+        file_ext = os.path.splitext(filename)[1]
+        #agregar validación de si no hay archivo
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_file(uploaded_file):
+            print("Error controlado")
+            abort(400)
+        basedir = os.path.abspath(os.path.dirname(__file__)) #Obtener el directorio actual
+        uploaded_file.save(os.path.join(basedir,app.config['UPLOAD_PATH'], filename)) #Guardar una copia temporal del archivo subido
+    return redirect(url_for('main', name=filename))
+
+@app.route('/main/<name>')
+def main(name):
+    f = Read_File(name)
+    age = Edad(f)[0]
+    if age != 0:
+        print("El paciente tiene:" ,age, "años")
+        #Funcion que imprime la edad
+    else:
+        print("No tengo la edad")
+        #Funcion que pide la edad
+    return f
+    
+#Funcion main driver
+if __name__ == '__main__':
+    webbrowser.open_new("http://127.0.0.1:5000") #Abrir la pagina principal en el navegador cuando se corre la app
+    app.run() #Correr la aplicación en el servidor local
+
+#https://werkzeug.palletsprojects.com/en/2.1.x/serving/#shutting-down-the-server
--- test.py
+++ test.py
@@ -1,85 +1,35 @@
-from flask import Flask, redirect, url_for, request, render_template, session, abort #Framework que permite crear aplicaciones web
-from werkzeug.utils import secure_filename #validar el archivo -> "Never trust user input"
-import os #usar funcionalidades dependientes del sistema operativo
 import aspose.words as aw #Lectura de archivos
-import webbrowser #Manejar el navegador
-import filetype
+from nltk.corpus import wordnet as wn
 
-#Validar que el archivo sea del tipo permitido (sin importar la extension que tenga)
-def validate_file(file):
-    header = file.read()
-    file.seek(0)
-    format = filetype.guess(header)
-    if (format is None):
-        return None
-    format = format.extension
-    if (format != "doc") and (format != "docx"):
-        return None
-    return '.' + format
-
-#Leer la copia local del archivo que se subió
-def Read_File(name):
-    f = []
-    text = []
-    basedir = os.path.abspath(os.path.dirname(__file__)) #Obtener el directorio actual
-    path = "".join([basedir,"\\static\\uploads\\", name]) #Obtener el directorio del archivo temporal
-    doc = aw.Document(path) # Cargar el archivo a leer
+f = []
+s = []
+text = []
+doc = aw.Document("C:\\Users\\danyg\\Documents\\Internado\\prueba.doc") # Cargar el archivo a leer
     # Leer el contenido de los parrafos tipo nodo
-    for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
-        paragraph = paragraph.as_paragraph()
-        p = paragraph.to_string(aw.SaveFormat.TEXT)
-        p = p.replace("\\", "/").replace('"','\\"').replace("'","\'") #Escapar caracteres especiales
-        p = p.replace('\n', '').replace('\r', '') #Eliminar saltos de linea y el retorno de carro
-        f.append(p)
-    #Eliminar el texto adicional que agrega la libreria aspose.words
-    size = len(f)
-    for x in range(1,size-2):
-        text.append(f[x])
-    
-    return text
+for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
+    paragraph = paragraph.as_paragraph()
+    p = paragraph.to_string(aw.SaveFormat.TEXT)
+    p = p.replace("\\", "/").replace('"','\\"').replace("'","\'") #Escapar caracteres especiales
+    p = p.replace('\n', '').replace('\r', '') #Eliminar saltos de linea y el retorno de carro
+    f.append(p)
+size = len(f)
+for x in range(1,size-2):
+    s.append(f[x])
+text = s[0].split(".") #Separar en oraciones
 
-# _.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.
+syn = wn.synonyms('INFARTO', lang='spa')
+print("Synonym: ",syn,"\n")
 
-# CONSTRUCTOR DE FLASH
-app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 #Limitar archivos a maximo 1MB
-app.config['UPLOAD_PATH'] = r'./static/uploads' #Path al que se subira la copia temporal de los archivos a ser procesados
-app.config['UPLOAD_EXTENSIONS'] = ['.doc', '.docx', '.txt'] #Extensiones permitidas
+syn = wn.lemmas('Infarto', lang='spa')
+print("Lemmas: ",syn)
+print("Lemmas type: ",type(syn[0]),"\n")
 
-#Generar la home page
-@app.route('/')
-def index():
-    return render_template('index.html')
- 
-#Recibir el archivo subido
-@app.route('/index', methods=['POST'])
-def load():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename) #validar el nombre del archivo 
-    if filename != '': #validar que si se subió un archivo
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_file(uploaded_file):
-            print("Error controlado")
-            abort(400)
-        basedir = os.path.abspath(os.path.dirname(__file__)) #Obtener el directorio actual
-        uploaded_file.save(os.path.join(basedir,app.config['UPLOAD_PATH'], filename)) #Guardar una copia temporal del archivo subido
-    return redirect(url_for('main', name=filename))
+syn = wn.synsets("Infarto", lang='spa')
+print("Synsets: ",syn)
+eng = syn[0].name()
+print("Synset name: ",eng)
+print("Synset name type: ",type(eng))
+eng = syn[0].lemma_names('spa')
+print("Lemma names: ",eng,"\n")
 
-@app.route('/main/<name>')
-def main(name):
-    f = Read_File(name)
-    return f
-    
-#Funcion main driver
-if __name__ == '__main__':
-    webbrowser.open_new("http://127.0.0.1:5000") #Abrir la pagina principal en el navegador cuando se corre la app
-    app.run() #Correr la aplicación en el servidor local
-
-#hacer una función ligada a JS que detecte cuando se cierre la ventana
-# JS -> window.unonload
-# https://werkzeug.palletsprojects.com/en/2.1.x/serving/#shutting-down-the-server
-# import os
-# os.system("comando de la terminal")
-#os.kill
-# os.kill(CTRL_C_EVENT)
+print("Definition: ",syn[0].definition())
